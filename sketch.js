846d3bba72947a014ec1f18a79f4b0c400c93a86
sakethjale <84376881+sakethjale@users.noreply.github.com>
2021-06-25 19:26:18 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,11 +6,12 @@ var engine,world;
 var house;
 var ball
 var balls=[]
-var balls2=[]
-var bg="img.png"
-var backGroundImg
+//var balls2=[]
+var bg
+//var backGroundImg
 function preload (){
-backGround()
+//backGround()
+bg=loadImage("snow2.jpg")
 }
 
 function setup() {
@@ -19,77 +20,84 @@ function setup() {
   engine=Engine.create();
   world=engine.world
 
-  backGround()
+ // backGround()
 
 }
 
 function draw() {
-  if(backGroundImg)
-  background(backGroundImg)  
+  //if(backGroundImg)
+  background(bg)  
   Engine.update(engine);
 
-if(hour>=12)
-{
-  textSize(30)
-  strokeWeight(30)
-  stroke("red")
-   text("Time : "+ hour%12 + " PM", 600,100); 
-}
-else if(hour==0)
-{
-  textSize(30)
-  strokeWeight(30)
-  stroke("orange")
- text("Time : 12 AM",600,100); 
-}
-else
-{
-  textSize(30)
-  strokeWeight(30)
-  stroke("green")
- text("Time : "+ hour%12 + " AM", 600,100);
-}
-
-
-if(hour>=12)
-{
   if(frameCount%1===0){
-    balls.push(new Ball(random(width/1-1200,width/1+1200),40,40))
-  }
-  for (var j = 0; j < balls.length; j++) {
-    balls[j].display();
-}
-}
-else 
-{
-  if(frameCount%1===0){
-    balls2.push(new Ball2(random(width/1-1200,width/1+1200),40,40))
-  }
-  for (var j = 0; j < balls2.length; j++) {
-    balls2[j].display();
-}
-}
+         balls.push(new Ball(random(width/1-1200,width/1+1200),40,40))
 
- drawSprites()
+  }
+  for(var j=0;j< balls.length;j++){
+  balls[j].display();
+  }
+// if(hour>=12)
+// {
+//   textSize(30)
+//   strokeWeight(30)
+//   stroke("red")
+//    text("Time : "+ hour%12 + " PM", 600,100); 
+// }
+// else if(hour==0)
+// {
+//   textSize(30)
+//   strokeWeight(30)
+//   stroke("orange")
+//  text("Time : 12 AM",600,100); 
+// }
+// else
+// {
+//   textSize(30)
+//   strokeWeight(30)
+//   stroke("green")
+//  text("Time : "+ hour%12 + " AM", 600,100);
+// }
+
+
+// if(hour>=12)
+// {
+//   if(frameCount%1===0){
+//     balls.push(new Ball(random(width/1-1200,width/1+1200),40,40))
+//   }
+//   for (var j = 0; j < balls.length; j++) {
+//     balls[j].display();
+// }
+// }
+// else 
+// {
+//   if(frameCount%1===0){
+//     balls2.push(new Ball2(random(width/1-1200,width/1+1200),40,40))
+//   }
+//   for (var j = 0; j < balls2.length; j++) {
+//     balls2[j].display();
+// }
+// }
+
+//  drawSprites()
   
  
-}
-async function backGround(){
-  var respon=await fetch("http://worldtimeapi.org/api/timezone/asia/kolkata");
-  var responJSON=await respon.json();
-  var day=responJSON.datetime;
+// }
+// async function backGround(){
+//   var respon=await fetch("http://worldtimeapi.org/api/timezone/asia/kolkata");
+//   var responJSON=await respon.json();
+//   var day=responJSON.datetime;
   
   
   
-  hour=day.slice(11,13)
+//   hour=day.slice(11,13)
   
-  console.log(hour)
+//   console.log(hour)
 
-  if(hour>=12&&hour<=24){
-bg="snow2.jpg"
-  }
-  else{
-    bg="land.jpg"
-  }
-  backGroundImg=loadImage(bg)
-}
+//   if(hour>=12&&hour<=24){
+// bg="snow2.jpg"
+//   }
+//   else{
+//     bg="land.jpg"
+//   }
+//   backGroundImg=loadImage(bg)
+ }
